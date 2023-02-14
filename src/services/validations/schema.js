7bc0a67cb4a840444ae58dca69d70b5e59cc0785
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-02-14 19:24:20 -0300

    Create: Cria validações para inserção de novo produto

--- src/services/validations/schema.js
+++ src/services/validations/schema.js
@@ -3,7 +3,7 @@ const Joi = require('joi');
 const idSchema = Joi.number().integer().required();
 
 const addProductSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string().min(5).required(),
 });
 
 module.exports = {
