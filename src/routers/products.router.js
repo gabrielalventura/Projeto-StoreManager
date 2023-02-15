bbcd06a7fc1644300a7fbf0303fece6d9a1b7b91
Gabriela Ventura <gabrielaleitev@gmail.com>
2023-02-15 11:58:00 -0300

    Create: Cria middleware para validar inserção do nome, garantindo que apareça o erro 400 quando o nome do produto não for inserido

--- src/routers/products.router.js
+++ src/routers/products.router.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { productsController } = require('../controllers');
+const validateName = require('../middlewares/middleware.product');
 
 const router = express.Router();
 
@@ -7,6 +8,6 @@ router.get('/', productsController.listProducts);
 
 router.get('/:id', productsController.listProductsById);
 
-router.post('/', productsController.createProduct);
+router.post('/', validateName, productsController.createProduct);
 
 module.exports = router;
